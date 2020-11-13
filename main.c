cd59454ffbe47ac3616b52318f475a9a321332d2
U-DESKTOP-S43NL97\badmin <appswert@gmail.com>
2020-11-13 19:40:51 +0100

    fix invalid read

--- main.c
+++ main.c
@@ -5,12 +5,13 @@
 //void loadMeshWithId()
 
 int main(int argc, char** argv){
+    dprintf(DBGT_INFO,"%d,%d,%d,%d",sizeof(uint32_t),sizeof(void*),sizeof(void**),sizeof(unsigned int));
     struct DynamicList* meshID=stringToUTF32Dynlist("Cylinder-mesh");
 
     FILE* cylinderDaeFileP=fopen("./res/kegel_ohne_camera.dae","rb");
     struct xmlTreeElement* xmlDaeRootP=0;
     readXML(cylinderDaeFileP,&xmlDaeRootP);
-    struct xmlTreeElement* xmlColladaElementP=getNthSubelement(xmlDaeRootP,0);
+    /*struct xmlTreeElement* xmlColladaElementP=getNthSubelement(xmlDaeRootP,0);
     struct xmlTreeElement* xmlLibGeoElementP=getFirstSubelementWith_freeArg2345(xmlColladaElementP,stringToUTF32Dynlist("library_geometries"),NULL,NULL,NULL,0);
     struct xmlTreeElement* xmlGeoElementP=getFirstSubelementWith_freeArg2345(xmlLibGeoElementP,stringToUTF32Dynlist("geometry"),stringToUTF32Dynlist("id"),DlDuplicate(meshID),NULL,0); //does  not work?
     struct xmlTreeElement* xmlMeshElementP=getFirstSubelementWith_freeArg2345(xmlGeoElementP,stringToUTF32Dynlist("mesh"),NULL,NULL,NULL,0);
@@ -54,5 +55,5 @@ int main(int argc, char** argv){
     if(xmlPositionsFloatP->content->type!=dynlisttype_utf32chars){dprintf(DBGT_ERROR,"Invalid content type");return 0;}
     struct DynamicList* PositionsDlP=utf32dynlistToFloats_freeArg123(createCharMatchList(4,' ',' ','\t','\t'),createCharMatchList(4,'e','e','E','E'),createCharMatchList(2,'.','.'),xmlPositionsFloatP->content);
     dprintf(DBGT_INFO,"Model has %d position coordinates",PositionsDlP->itemcnt);
-
+*/
 }
